edf423a0524ee701c2c634cd66e26a7127471f87
JaniPaa <janimp@metropolia.fi>
2018-11-29 20:53:46 +0200

    Creating tables and some other functionalities

--- js/mainsivu_js.js
+++ js/mainsivu_js.js
@@ -1,7 +1,10 @@
 window.onscroll = function (){
 	pageScroll();
 };
-
+window.onload = function() {
+	document.getElementById('hidebutton').style.visibility = 'hidden';
+	showEvents();
+};
 var navbar = document.getElementsByClassName('topnav');
 var sticky = navbar.offsetTop;
 
@@ -17,6 +20,58 @@ function easterEgg() {
 	alert('You found Easter egg!');
 }
 
+function tableCreate() {
+	document.getElementById('hidebutton').style.visibility = 'visible';
+	var place = document.getElementById('tablespace');
+	var tbl = document.createElement('table');
+	tbl.style.width = '20%';
+	tbl.setAttribute('border', '1');
+	var tbdy = document.createElement('tbody');
+	for (var i = 0; i < 1; i++) {
+		var tr = document.createElement('tr');
+		for (var j = 0; j < 2; j++) {
+			if (i === 3 && j === 3) {
+				break;
+			} else {
+				var td = document.createElement('td');
+				td.appendChild(document.createTextNode('testi'));
+				tr.appendChild(document.createTextNode('Event: '));
+				tr.appendChild(td);
+			}
+		}
+		tbdy.appendChild(tr);
+	}
+	tbl.appendChild(tbdy);
+	place.appendChild(tbl);
+	var button = document.getElementById('eventbutton');
+	button.parentNode.removeChild(button);
+}
+function hideEvents() {
+	var x = document.getElementById('tablespace');
+	if(x.style.display === 'none'){
+		document.getElementById('hidebutton').innerHTML = 'Hide matches';
+		x.style.display = 'block';
+	}else {
+		document.getElementById('hidebutton').innerHTML = 'Show matches';
+		x.style.display = 'none';
+	}
+}
+
+function showEvents() {
+	var table = document.getElementById('myTable');
+	for(var i = 0; i<10; i++){
+		var row = table.insertRow(0);
+		var cell1 = row.insertCell(0);
+		cell1.innerHTML = 'Eventin nimi';
+	}
+}
 
 
+var ypos;
+var image;
+window.addEventListener('scroll', function() {
+	ypos = window.pageYOffset;
+	image = document.getElementById('centerblock');
+	image.style.top = ypos * 0.8 + 'px';
+});
 
